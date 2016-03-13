54ad0709c0947b771b564109ebc3663fb291ab53
Rade Milovic <r.milovic90@gmail.com>
2016-03-13 23:34:52 +0100

    Adds WinTail example.

---
+++ src/WinTail/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Runtime.InteropServices;
+using System.Reflection;
+
+[assembly: AssemblyTitle("WinTail")]
+[assembly: AssemblyDescription("Windows version of tail command from *NIX systems.")]
+[assembly: AssemblyVersion("1.0.0")]
+
+[assembly: ComVisible(false)]
